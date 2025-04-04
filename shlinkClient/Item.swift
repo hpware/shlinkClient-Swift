cbf2181639bc0e62e671900c26091c25b24e4c85
吳元皓 <hpware3@gmail.com>
2025-04-04 21:18:22 +0800

    Initial Commit

---
+++ shlinkClient/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  shlinkClient
+//
+//  Created by Howard Wu on 2025/4/4.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ shlinkClientTests/shlinkClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  shlinkClientTests.swift
+//  shlinkClientTests
+//
+//  Created by Howard Wu on 2025/4/4.
+//
+
+import Testing
+@testable import shlinkClient
+
+struct shlinkClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
