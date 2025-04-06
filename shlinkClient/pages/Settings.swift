2c61281f21a7cb39b71ce2fd279660c4f1c4b967
吳元皓 <hpware3@gmail.com>
2025-04-06 10:54:26 +0800

    Add initial Setup and Settings pages with basic structure

---
+++ shlinkClient/pages/Settings.swift
@@ -0,0 +1,5 @@
+struct SettingsPage {
+    var body: some View {
+        
+    }
+}
---
+++ shlinkClient/pages/Setup.swift
@@ -0,0 +1,5 @@
+struct SetupPage: View {
+    var body: some View {
+        Text("Hello! Ready to setup your account?")
+    }
+}
